38fdcf8b4800c1c84e3ae424ecbf9b9e808e9720
Bahir Taraki <btara001@gold.ac.uk>
2021-10-21 21:22:44 +0100

    finished the productContorller

--- src/main/java/com/sparta/bt/northwindrest/controllers/ProductController.java
+++ src/main/java/com/sparta/bt/northwindrest/controllers/ProductController.java
@@ -5,9 +5,9 @@ import com.sparta.bt.northwindrest.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
@@ -21,23 +21,98 @@ public class ProductController {
 
     @GetMapping("/products")
     @ResponseBody
-    public List<ProductEntity> getAllProducts(@RequestParam(required = false)Integer id){
-        List<ProductEntity> foundProducts = new ArrayList<>();
-        if(id == null){
-            return productRepository.findAll();
-        }
-        else if(id != null){
-            for(ProductEntity productEntity: productRepository.findAll()){
-                if(productEntity.getId()==id){
-                    foundProducts.add(productEntity);
-                }
-            }
-        }
-        return foundProducts;
+    public List<ProductEntity> getAllProducts() {
+        return productRepository.findAll();
     }
 
-//    @GetMapping("/products/{id}")
-//    public Optional<ProductEntity> getProductsByID(@PathVariable Integer id){
-//        return productRepository.findById(id);
-//    }
+    @GetMapping("/products/id={id}")
+    public Optional<ProductEntity> getProductsByID(@PathVariable Integer id){
+        return productRepository.findById(id);
+    }
+
+    @GetMapping("/products/name={name}")
+    public List<ProductEntity> getProductsByName(@PathVariable String name){
+        return productRepository.findAll()
+                .stream()
+                .filter(customerEntity -> customerEntity.getProductName().equals(name))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/products/supplierID={supplierID}")
+    public List<ProductEntity> getProductsBySupplierID(@PathVariable Integer supplierID){
+        return productRepository.findAll()
+                .stream()
+                .filter(customerEntity -> customerEntity.getSupplierID().equals(supplierID))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/products/categoryID={categoryID}")
+    public List<ProductEntity> getProductsBycategoryID(@PathVariable Integer categoryID){
+        return productRepository.findAll()
+                .stream()
+                .filter(customerEntity -> customerEntity.getCategoryID().equals(categoryID))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/products/quantityPerUnit={quantityPerUnit}")
+    public List<ProductEntity> getProductsByQuantityPerUnit(@PathVariable String quantityPerUnit){
+        return productRepository.findAll()
+                .stream()
+                .filter(customerEntity -> customerEntity.getQuantityPerUnit().equals(quantityPerUnit))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/products/unitPrice={unitPrice}")
+    public List<ProductEntity> getProductsByUnitPrice(@PathVariable Double unitPrice){
+        return productRepository.findAll()
+                .stream()
+                .filter(customerEntity -> customerEntity.getUnitPrice().equals(unitPrice))
+                .collect(Collectors.toList());
+    }
+    @GetMapping("/products/unitPrice>{unitPrice}")
+    public List<ProductEntity> getProductsLargeThenUnitPrice(@PathVariable Double unitPrice){
+        return productRepository.findAll()
+                .stream()
+                .filter(customerEntity -> customerEntity.getUnitPrice()>(unitPrice))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/products/unitPrice<{unitPrice}")
+    public List<ProductEntity> getProductsLowerThenUnitPrice(@PathVariable Double unitPrice){
+        return productRepository.findAll()
+                .stream()
+                .filter(customerEntity -> customerEntity.getUnitPrice()<(unitPrice))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/products/unitInStock={unitInStock}")
+    public List<ProductEntity> getProductsEqualtoUnitInStock(@PathVariable Integer unitInStock){
+        return productRepository.findAll()
+                .stream()
+                .filter(customerEntity -> customerEntity.getUnitsInStock()==(unitInStock))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/products/unitInStock>{unitInStock}")
+    public List<ProductEntity> getProductsGreaterThenUnitInStock(@PathVariable Integer unitInStock){
+        return productRepository.findAll()
+                .stream()
+                .filter(customerEntity -> customerEntity.getUnitsInStock()>(unitInStock))
+                .collect(Collectors.toList());
+    }
+    @GetMapping("/products/unitInStock<{unitInStock}")
+    public List<ProductEntity> getProductsLowerThenUnitPrice(@PathVariable Integer unitInStock){
+        return productRepository.findAll()
+                .stream()
+                .filter(customerEntity -> customerEntity.getUnitsInStock()<(unitInStock))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/products/discontinued={discontinued}")
+    public List<ProductEntity> getProductsThatAreDiscontinued(@PathVariable Integer discontinued){
+        return productRepository.findAll()
+                .stream()
+                .filter(customerEntity -> customerEntity.getDiscontinued()==(discontinued))
+                .collect(Collectors.toList());
+    }
 }
